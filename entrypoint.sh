edebdf01b18b5eb99ff43cf631640cf16d855783
Daniel <dbelendelrio@gmail.com>
2021-04-04 21:39:08 +0200

    Fixes en configuracion y entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-# Update suricata rules
-suricata-update
+# Update suricata rules (first time)
+if [ ! -f /var/run/suricata.pid ]; then
+    suricata-update
+fi
 
 # Start cron
 crond
@@ -11,4 +13,7 @@ crontab /etc/crontabs/suricata-update-cron
 
 # Start suricata (and pass extra arguments from CMD)
 # pidfile is required for logrotate
+if [ -f /var/run/suricata.pid ]; then
+    rm -f /var/run/suricata.pid
+fi
 suricata -c /etc/suricata/suricata.yaml --pidfile /var/run/suricata.pid "$@"
